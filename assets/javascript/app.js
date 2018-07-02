af587daa58c609f8f4ba83364b49e9d60c30ee1b
pjoyce5231 <pjoyce5231@gmail.com>
2018-07-02 14:21:39 -0400

    first addition

---
+++ assets/javascript/app.js
@@ -0,0 +1,37 @@
+//variables(create an array with each question having it's own object)
+var questions = ["Spongebob lives in a pineapple under the sea.", "Squidward owns the Krusty Krab and loves money.", "Mr. Krabs plays the clarinet.", "Spongebob's favorite activity is Jellyfishing.", "Plankton runs the restaurant called the Chum Bucket."]
+//generate random question
+
+//user guesses true or false
+
+//new question appears on the screen
+
+//after new question appears on the screen, the wins and losses
+//will be tracked
+
+// var questions = [{
+//         question1: "Who lives in a pineapple under the sea?",
+//         answers: [1, 2, 3, 4], //1Sandy, 2Patrick, 3Spongebob, 4Plankton
+//         rightAnswer: 3,
+//     },
+//     {
+//         question2: "Who owns the Krusty Krab and loves money?",
+//         answers: [1, 2, 3, 4], //1Sandy, 2Mr. Krabs, 3Larry the Lobster, 4Squidward
+//         rightAnswer: 2,
+//     },
+//     {
+//         question3: "Who plays the clarinet in the show?",
+//         answers: [1, 2, 3, 4], //1squidward ,2gary the snail, 3Mrs. Puff, 4Karen
+//         rightAnswer: 1,
+//     },
+//     {
+//         question4: "What is Spongebob's favorite activity?",
+//         answers: [1, 2, 3, 4], //1the fry cook games, 2fishing, 3driving, 4Jellyfishing
+//         rightAnswer: 4,
+//     }
+//     {
+//         question5: "Who runs the restaurant called the Chum Bucket?",
+//         answers: [1, 2, 3, 4], //1sandy, 2Patrick, 3Spongebob, 4Plankton
+//         rightAnswer: 4,
+//     }
+// ]
